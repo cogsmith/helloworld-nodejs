4dcc46d06d7ae012b462d4062fc81d5a3dc43b62
DAC <dac@cogsmith.com>
2021-02-17 01:49:41 -0500

    .

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const host = process.argv[3]||'0.0.0.0'; const port = process.argv[2]||80;
+const host = process.argv[3] || '0.0.0.0'; const port = process.argv[2] || 80;
 let msg = 'HELLOWORLD'; msg += " @ " + host + ':' + port;
-const handler = function (req, res) { res.writeHead(200); res.end(msg+"\n"); };
+const handler = function (req,res) { res.writeHead(200); res.end(msg+"\n"); };
 const server = require('http').createServer(handler); server.listen(port,host);
 console.log(msg);
