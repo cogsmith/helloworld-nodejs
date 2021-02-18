adbf00075f2012ff8dae87be025f0842484381e2
DAC <dac@cogsmith.com>
2021-02-17 19:37:11 -0500

    .

--- README.md
+++ README.md
@@ -9,23 +9,3 @@ https://github.com/COGSMITH/helloworld-nodejs
 https://hub.docker.com/r/cogsmith/helloworld-nodejs
 
 ---
-
-~~~
-# DOCKER-RUN
-docker run -d --name helloworld -p 127.0.0.1:80:80 cogsmith/helloworld-nodejs ; sleep 1 ; curl http://localhost:80
-
-# OUTPUT
-HELLOWORLD: d8e6338341ff @ 0.0.0.0:80 @ LOCALHOST @ 2021-02-17T09:02:04.404Z
-~~~
-
----
-
-~~~
-# DOCKER-RUN
-docker run -d --name helloworld -p 127.0.0.1:99:80 cogsmith/helloworld-nodejs ; sleep 1 ; curl http://127.0.0.1:99
-
-# OUTPUT
-HELLOWORLD: e1d8e42150d3 @ 0.0.0.0:80 @ 127.0.0.1:99 @ 2021-02-17T23:48:04.394Z
-~~~
-
----
--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const port = process.argv[2] || 80; const host = process.argv[3] || '0.0.0.0';
-const msg = function (req) { let msg = 'HELLOWORLD'; msg += ': '+require('os').hostname()+' @ '+host+':'+port+' @ '+(req?req.headers.host.toUpperCase():'_')+' @ '+new Date().toISOString(); return msg; };
+const msg = function (req) { let msg = 'HELLOWORLD'; msg += ': '+require('os').hostname()+' @ '; if (req) { msg += host+':'+port+' @ '+req.headers.host.toUpperCase()+' @ '; }; msg += new Date().toISOString(); return msg; };
 const handler = function (req,res) { res.writeHead(200); res.end(msg(req)+"\n"); };
-const server = require('http').createServer(handler); server.listen(port,host);
+if (port!='_') { const server = require('http').createServer(handler); server.listen(port,host); }
 console.log(msg());
