67aa95fdcc45ca00d5e5a3fd669046d41a226a24
DAC <dac@cogsmith.com>
2021-03-12 23:10:33 -0500

    .

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const port = process.env.PORT || '_'; const host = process.env.HOST || '0.0.0.0';
-const msg = function (req) { let msg = 'HELLOWORLD'; msg += ': '+require('os').hostname().toUpperCase()+' @ '; if (req) { msg += host+':'+port+' @ '+req.headers.host.toUpperCase()+' @ '; }; msg += new Date().toISOString(); return msg; };
+const port = process.env.PORT||process.argv[2]||'_'; const host = process.env.HOST||process.argv[3]||'0.0.0.0';
+const msg = function (req) { let msg = 'HELLOWORLD'; msg += ': '+require('os').hostname().toUpperCase()+' @ '+host+':'+port+' @ '; if (req) { msg += req.headers.host.toUpperCase()+' @ '; }; msg += new Date().toISOString(); return msg; };
 if (port!='_') { const server = require('http').createServer((req,res) => { res.writeHead(200); res.end(msg(req)+"\n"); }); server.listen(port,host); }
 console.log(msg());
